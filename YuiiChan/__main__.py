89068aa9136365706f667fc9ed83a3357fa4f0d2
rshero <52062460+rshero@users.noreply.github.com>
2020-12-26 09:29:06 -0800

    oops

--- YuiiChan/__main__.py
+++ YuiiChan/__main__.py
@@ -235,7 +235,7 @@ def start(update: Update, context: CallbackContext):
             )
     else:
         update.effective_message.reply_text(
-            "Well I;m alive!\n<b>Working since:</b> <code>{}</code>".format(
+            "Well I'm alive!\n<b>Working since:</b> <code>{}</code>".format(
                 uptime),
             parse_mode=ParseMode.HTML,
         )
