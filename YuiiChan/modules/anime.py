9f0d31059c6a61b33c5ed4145c5a8ac0bece5175
rshero <52062460+rshero@users.noreply.github.com>
2020-12-28 11:01:53 +0530

    Update anime.py

--- YuiiChan/modules/anime.py
+++ YuiiChan/modules/anime.py
@@ -583,7 +583,7 @@ Get information about anime, manga or characters from [AniList](anilist.co).
  • `/kaizoku <anime>`*:* search an anime on animekaizoku.com
  • `/kayo <anime>`*:* search an anime on animekayo.com
  • `/airing <anime>`*:* returns anime airing info.
-
+ • `/quote `*:* returns an random anime quote.
  """
 
 ANIME_HANDLER = DisableAbleCommandHandler("anime", anime)
